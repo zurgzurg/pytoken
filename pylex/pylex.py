c765b3a679c39e85c376a02ce9fb21ed987db962
ramb <ramb@2ada0880-784b-0410-adfc-ef60aad23146>
2007-06-20 13:27:59 +0000

    now have some tests working with | char

--- pylex/pylex.py
+++ pylex/pylex.py
@@ -1,22 +1,22 @@
 class node(object):
     next_avail_num = 0
+    __slots__ = ["name", "next_tbl", "action", "is_accepting", "priority"]
     def __init__(self):
         self.name         = "Node_%d" % node.next_avail_num
         node.next_avail_num += 1
-        self.nfa_tbl      = []
         self.next_tbl     = {}
         self.action       = None
         self.is_accepting = False
         self.priority     = None
         return
 
-    def must_get_next_node(self, ch_str):
-        assert type(ch_str) is str
-        ch0 = ch_str[0]
+    def must_get_next_node(self, ch_str_or_tup):
+        assert type(ch_str_or_tup) is str or type(ch_str_or_tup) is tuple
+        ch0 = ch_str_or_tup[0]
         if not self.next_tbl.has_key(ch0):
             self.next_tbl[ch0] = node()
         the_node = self.next_tbl[ch0]
-        for ch in ch_str[1:]:
+        for ch in ch_str_or_tup[1:]:
             self.next_tbl[ch] = the_node
         return the_node
 
@@ -26,12 +26,23 @@ class node(object):
         result = self.next_tbl[ch]
         return result
 
+    def num_out_edges(self):
+        return len(self.next_tbl.items())
+
     def show_tbl(self):
         print "Node table"
         for ch, nxt in self.next_tbl.items():
             print "    ", ch, "--->", nxt
         print "Done."
         return
+
+    def __str__(self):
+        result = [self.name]
+        result.append("is_accepting=" + str(self.is_accepting))
+        result.append("priority=" + str(self.priority))
+        for ch, node in self.next_tbl.items():
+            result.append("ch %s --> node %s" % (ch, node.name))
+        return "\n".join(result)
 
     pass
 
@@ -45,7 +56,7 @@ class lexer(object):
         self.cur_pattern = None
         return
 
-    def token(self, pat, action):
+    def define_token(self, pat, action):
         self.pats.append((pat, action))
         return
 
@@ -55,38 +66,64 @@ class lexer(object):
     def compile(self):
         for pat, action in self.pats:
             self.compile_one_pat(self.start, pat, action)
-        return
 
-    def get_next_chars(self, pat):
-        ch = pat[0]
-        if ch != "[":
-            pat_next = pat[1:]
-            if len(pat_next) == 0:
-                pat_next = None
-            return (ch, pat_next)
-        idx = pat.find("]")
-        if idx == -1:
-            raise RuntimeError, "Unable to find closing ']' char " \
-                  + "in pattern: " + self.cur_pattern
-        cclass = pat[1:idx]
-        pat_next = pat[idx+1:]
-        if len(pat_next) == 0:
-            pat_next = None
-        return (cclass, pat_next)
+        return
 
     def compile_one_pat(self, node, pat, action):
         cur_pat  = pat
         cur_node = node
 
         while cur_pat:
-            ch, cur_pat = self.get_next_chars(cur_pat)
-            cur_node = cur_node.must_get_next_node(ch)
+            ch_info, cur_pat = self.get_next_chars(cur_pat)
+            cur_node = cur_node.must_get_next_node(ch_info)
             pass
 
         cur_node.action = action
         cur_node.is_accepting = True
         return
         
+    def get_next_chars(self, pat):
+        ch = pat[0]
+
+        ch2 = None
+        if len(pat) > 1:
+            ch2 = pat[1]
+
+        if ch != "[" and ch2 != '|' and ch != '(':
+            pat_next = pat[1:]
+            if len(pat_next) == 0:
+                pat_next = None
+            return (ch, pat_next)
+
+        if ch == '[':
+            idx = pat.find("]")
+            if idx == -1:
+                raise RuntimeError, "Unable to find closing ']' char " \
+                      + "in pattern: " + self.cur_pattern
+            cclass = pat[1:idx]
+            pat_next = pat[idx+1:]
+            if len(pat_next) == 0:
+                pat_next = None
+            return (cclass, pat_next)
+
+        if ch == '(':
+            idx2 = pat.find(')')
+            if idx2 == -1:
+                raise RuntimeError, "No matching close ')' found"
+            sub = pat[1:idx2]
+            pat_next = pat[idx2+1:]
+            return (sub, pat_next)
+
+        assert ch2 == '|'
+        if len(pat) > 2:
+            ch3 = pat[2]
+            pat_next = pat[3:]
+        else:
+            raise RuntimeError, "Illegal use of alternation character | at " \
+                  + "end of string." + self.cur_pattern
+            
+        return ((ch, ch3), pat_next)
+
     #######################################
     ##
     #######################################
--- pylex/utest.py
+++ pylex/utest.py
@@ -6,6 +6,12 @@ import pdb
 
 import pylex
 
+class lex_test(unittest.TestCase):
+    def check_token(self, obj, txt, exp):
+        tok = obj.parse(txt)
+        self.assert_(tok == exp)
+        return
+
 class simple01(unittest.TestCase):
     def runTest(self):
         return True
@@ -21,7 +27,7 @@ class simple02(unittest.TestCase):
 class simple03(unittest.TestCase):
     def runTest(self):
         obj = pylex.lexer()
-        obj.token("hello", 1)
+        obj.define_token("hello", 1)
         obj.compile()
         return True
     pass
@@ -29,7 +35,7 @@ class simple03(unittest.TestCase):
 class simple04(unittest.TestCase):
     def runTest(self):
         obj = pylex.lexer()
-        obj.token("hello", 1)
+        obj.define_token("hello", 1)
         obj.compile()
         val = obj.parse("hello")
         self.assert_(val == 1)
@@ -39,9 +45,9 @@ class simple04(unittest.TestCase):
 class simple05(unittest.TestCase):
     def runTest(self):
         obj = pylex.lexer()
-        obj.token("hello", 1)
-        obj.token(" ", None)
-        obj.token("world", 2)
+        obj.define_token("hello", 1)
+        obj.define_token(" ", None)
+        obj.define_token("world", 2)
         obj.compile()
 
         toks = obj.parse("hello world")
@@ -53,8 +59,8 @@ class simple05(unittest.TestCase):
 class simple06(unittest.TestCase):
     def runTest(self):
         obj = pylex.lexer()
-        obj.token("a", 1)
-        obj.token("aa", 2)
+        obj.define_token("a", 1)
+        obj.define_token("aa", 2)
         obj.compile()
 
         tok = obj.parse("a")
@@ -69,7 +75,7 @@ class simple06(unittest.TestCase):
 class simple07(unittest.TestCase):
     def runTest(self):
         obj = pylex.lexer()
-        obj.token("[abcd]", 1)
+        obj.define_token("[abcd]", 1)
         obj.compile()
 
         tok = obj.parse("a")
@@ -87,8 +93,8 @@ class simple07(unittest.TestCase):
 class simple08(unittest.TestCase):
     def runTest(self):
         obj = pylex.lexer()
-        obj.token("[abcd]", 1)
-        obj.token("g", 2)
+        obj.define_token("[abcd]", 1)
+        obj.define_token("g", 2)
         obj.compile()
 
         tok = obj.parse("a")
@@ -104,6 +110,87 @@ class simple08(unittest.TestCase):
 
         return True
     pass
+
+class simple09(unittest.TestCase):
+    def runTest(self):
+        obj = pylex.lexer()
+        obj.define_token("ab", 1)
+        obj.define_token("a|b", 2)
+        obj.compile()
+        
+        tok = obj.parse("ab")
+        self.assert_(tok == 1)
+        tok = obj.parse("b")
+        self.assert_(tok == 2)
+
+        return True
+
+class simple10(lex_test):
+    def runTest(self):
+        obj = pylex.lexer()
+        obj.define_token("ab", 1)
+        obj.define_token("a|b", 2)
+        obj.compile()
+        
+        self.check_token(obj, "a", 2)
+        self.check_token(obj, "ab", 1)
+        self.check_token(obj, "b", 2)
+        return True
+
+class simple11(lex_test):
+    def runTest(self):
+        obj = pylex.lexer()
+        obj.define_token("a|b", 2)
+        obj.compile()
+        
+        self.check_token(obj, "a", 2)
+        self.check_token(obj, "b", 2)
+
+        return True
+
+class simple12(lex_test):
+    def runTest(self):
+        obj = pylex.lexer()
+        obj.define_token("ca|ot", 2)
+        obj.compile()
+        
+        self.check_token(obj, "cat", 2)
+        self.check_token(obj, "cot", 2)
+
+        return True
+
+class simple13(lex_test):
+    def runTest(self):
+        obj = pylex.lexer()
+        obj.define_token("ca|ot", 2)
+        obj.compile()
+        self.assert_(obj.start.num_out_edges() == 1)
+        return
+
+class simple14(lex_test):
+    def runTest(self):
+        obj = pylex.lexer()
+        obj.define_token("ca|ot", 2)
+        obj.compile()
+        self.assert_(obj.start.num_out_edges() == 1)
+        return
+
+class simple15(lex_test):
+    def runTest(self):
+        obj = pylex.lexer()
+        obj.define_token("ca|ot", 2)
+        obj.compile()
+        n2 = obj.start.maybe_get_next_node('c')
+        self.assert_(n2.num_out_edges() == 2)
+        return
+
+class simple16(lex_test):
+    def runTest(self):
+        obj = pylex.lexer()
+        obj.define_token("(cat)", 2)
+        obj.compile()
+        self.check_token(obj, "cat", 2)
+        return
 
 if __name__=="__main__":
     unittest.main()
