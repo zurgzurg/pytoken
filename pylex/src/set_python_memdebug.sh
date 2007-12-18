2b44f9866d6df6933e5082a305071ef270f5b02d
ramb <ramb@2ada0880-784b-0410-adfc-ef60aad23146>
2007-12-18 03:35:40 +0000

    also need to remove PYTHON* type env vars

--- pylex/src/set_python_memdebug.sh
+++ pylex/src/set_python_memdebug.sh
@@ -13,3 +13,12 @@ function remove_python_from_path() {
 
 remove_python_from_path
 PATH=/custom/python-2.5.1-memdebug/bin:$PATH
+
+if [[ ${PYTHONPATH:-notset} =~ /custom/python ]]; then
+  echo "unsetting PYTHONPATH"
+  unset PYTHONPATH
+fi
+if [[ ${PYTHONHOME:-notset} =~ /custom/python ]]; then
+  echo "unsetting PYTHONHOME"
+  unset PYTHONHOME
+fi
