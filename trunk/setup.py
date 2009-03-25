c061041434135dbc2f6130d475c35a3fe79a0f52
rambham <rambham@2ada0880-784b-0410-adfc-ef60aad23146>
2009-03-25 13:39:37 +0000

    start of an example working with ply

--- trunk/setup.py
+++ trunk/setup.py
@@ -116,5 +116,5 @@ setup(name = 'pytoken',
       author_email = 'rambham@gmail.com',
       url = 'http://code.google.com/p/pytoken/',
       ext_modules = mlist,
-      py_modules = ['pytoken', 'pytoken_ply'],
+      py_modules = ['pytoken', 'pytoken_ply_lex'],
       cmdclass = {"clean" : clean} )
