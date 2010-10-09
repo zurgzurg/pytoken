a26cd03d57f4ba3073825b1d2afb915c2bb52e34
rambham <rambham@2ada0880-784b-0410-adfc-ef60aad23146>
2010-10-09 03:55:22 +0000

    starting to add compatability testing with the ply lexer

--- branches/arrays/pytoken_ply_lex.py
+++ branches/arrays/pytoken_ply_lex.py
@@ -1,62 +1,98 @@
 import sys
 import pytoken
 
-try:
-    import ply.lex
-    found_ply_lex = True
-except ImportError:
-    found_ply_lex = False
-    lex = None
-
 ##
 ## layer to make pytoken usable for the
 ## ply lexer/parser generator
 ##
 
-if found_ply_lex:
-    class lex(object):
-        def __init__(self):
-            self.pytok_lexer = pytoken.lexer()
-            self.pytok_state = pytoken.lexer_state()
+#####################################################
+class lex(object):
+    def __init__(self):
+        self.toks = self.get_tok_info()
+        self.lobj = self.make_lexer(self.toks)
 
-            func_pats = []
-            simple_pats = []
+        return
 
-            d = ply.lex.get_caller_module_dict(2)
-            for tok_name in d['tokens']:
-                tok_sym_name = "t_" + tok_name
-                tok_obj = d[tok_sym_name]
-                if type(tok_obj) is str:
-                    simple_pats.append((tok_sym_name, tok_obj))
-                else:
-                    regex = tok_obj.func_doc
-                    func_pats.append((tok_sym_name, tok_obj.co_firstlineno, regex))
+    def get_tok_info(self):
+        md = get_caller_module_dict(3)
+        if 'tokens' not in md:
+            raise RuntimeError, "Unable to find ply required list of tokens."
+        tok_names = md['tokens']
+        toks = [TokenInfo(tname) for tname in md['tokens']]
+        
+        for tinfo in toks:
+            tval = md['t_' + tinfo.name]
+            if type(tval) is str:
+                tinfo.regex = tval
 
+        return toks
 
-            def simple_sorter(tup1, tup2):
-                if len(tup1[1]) < len(tup2[1]):
-                    return -1
-                elif len(tup1[1]) > len(tup2[1]):
-                    return 1
-                return 0
+    def make_lexer(self, toks):
+        lobj = pytoken.lexer()
+        for idx, tinfo in enumerate(toks):
+            lobj.add_pattern(tinfo.regex, tok_func, tinfo.name)
+        lobj.compile_to_arrays()
+        return lobj
 
+    #####################
+    ### ply.lex api below
+    #####################
+    def input(self, obj):
+        self.lstate = pytoken.lexer_state();
+        self.lstate.set_input(obj)
+        return
+        
+    def token(self):
+        tok = self.lobj.get_token( self.lstate )
+        return tok
 
-            simple_pats.sort(simple_sorter)
+    pass
 
-            for sym, pat in simple_pats:
-                print pat, "-->", sym
-                self.pytok_lexer.add_pattern(pat, sym)
+def tok_func(txt, tname):
+    res = LexToken()
+    res.type = tname
+    res.value = txt
+    res.lineno = 1
+    res.lexpos = 0
+    return res
 
-            self.pytok_lexer.compile_to_machine_code()
+class TokenInfo:
+    def __init__(self, name):
+        self.name = name
+        self.regex = None
+        return
+    pass
 
-            return
+#####################################################
+class LexToken(object):
+    """Clone of the ply token object."""
+    def __init__(self):
+        self.type = None
+        self.value = None
+        self.lineno = None
+        self.lexpos = None
+        return
+    def __str__(self):
+        return "LexToken(%s,%r,%d,%d)" % \
+            (self.type,self.value,self.lineno,self.lexpos)
+    def __repr__(self):
+        return str(self)
+    pass
 
-        def input(self, txt):
-            self.pytok_state.set_input(txt)
-            return
+#####################################################
+#####################################################
+def get_caller_module_dict(levels):
+    try:
+        raise RuntimeError
+    except RuntimeError:
+        e,b,t = sys.exc_info()
+        f = t.tb_frame
+        while levels > 0:
+            f = f.f_back                   
+            levels -= 1
+        ldict = f.f_globals.copy()
+        if f.f_globals != f.f_locals:
+            ldict.update(f.f_locals)
 
-        def token(self):
-            tok = self.pytok_lexer.get_token(self.pytok_state)
-            return tok
-
-        pass
+        return ldict
